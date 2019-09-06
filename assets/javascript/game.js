e1a00706ecb0b35431fca97e5188146f24642e3f
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2019-09-05 23:51:53 -0700

    added some event.key logic and variables to write to the browser

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,47 @@
+// array of words for computer to use
+let words = [
+    "antelope", "albatross", "alligator", "armadillo", "badger", 
+    "bear", "beaver", "beetle", "bovine", "buffalo", "bushbuck", "butterfly", 
+    "capybara", "caribou", "chickadee", "coatimundi", "cormorant", "cougar", "crab", 
+    "dinosaur", "dingo", "dolphin", "donkey", "dragonfly", "duiker",
+    "eagle", "elephant",
+    "flamingo", 
+    "gazelle", "giraffe", "goat", 
+    "hawk", "heron", "horse", "hummingbird", "hyena", 
+    "ibis", "iguana", 
+    "jaguar",
+    "kangaroo", "kingfisher", "kitten", "kudu",
+    "lemming", "leopard", "lion", "lizard", "lynx", 
+    "magpie", "meerkat", "mockingbird", "mongoose", 
+    "otter", "orca", "oryx", "osprey", "ostrich", "oystercatcher", 
+    "pelican", "penguin", "puma", "peacock", "porcupine", "python", 
+    "quail", 
+    "raccoon", "rabbit", "rhinoceros", 
+    "skink", "squirrel", "sparrow", "spider", "starfish", "starling", "stilt", "stork", 
+    "tapir", "tarantula", "tern", "tortoise", "turkey",
+    "vulture", 
+    "warthog", "whale", "woodpecker",
+    "zebra"
+];
 
-// create array of words for computer to use
-
-var words = ["kitten", "giraffe", "alligator", "dinosaur", "bovine", "horse", "buffalo", "squirrel", "elephant", "eagle", "lion", "lynx", "porcupine", "python", "kangaroo", "tortoise", "badger", "beaver", "bear", "woodpecker", "armadillo", "albatross", "donkey", "magpie", "rhinoceros", "lizard", "skink", "otter", "capybara", "caribou", "chickadee", "flamingo", "cormorant", "cougar", "ibis", "dingo", "dolphin", "rabbit", "heron", "hyena", "leopard", "jaguar", "peacock", "mongoose", "vulture", "kingfisher", "bushbuck", "kudu", "duiker", "iguana", "hummingbird", "hawk", "tapir", "lemming", "stork", "mockingbird", "goat", "orca", "whale", "oryx", "osprey", "ostrich", "oystercatcher", "pelican", "penguin", "puma", "quail", "raccoon", "crab", "meerkat", "coatimundi", "antelope", "tern", "dragonfly", "butterfly", "sparrow", "spider", "beetle", "starfish", "starling", "stilt", "tarantula", "gazelle", "turkey", "warthog", "zebra"];
-
-// use Math.floor(Math.random() * arr.length) to generate current word
-
-var currentWord = words[Math.floor(Math.random() * words.length)].split("");
+// generate random word to guess and split it into an array
+let currentWord = words[Math.floor(Math.random() * words.length)].split("");
 console.log(currentWord);
 
 // emptyArray.length = word.length & for each index, value = "_"
+let guessedWord = document.getElementById("currentWord");
+let placeholderWord = [];
 
-var guessedWord = [];
+let wins = document.getElementById("Wins");
+wins = 0;
 
-for (var i = 0; i < currentWord.length; i++) {
-    guessedWord.push("_ ");
-    console.log(guessedWord);
+ 
+
+
+for (let i = 0; i < currentWord.length; i++) {
+    placeholderWord.push("_ ");
+    console.log(placeholderWord);
+    guessedWord.textContent = placeholderWord.join("");
 };
 
 // document.onkeyup stuff.  
@@ -22,17 +49,26 @@ for (var i = 0; i < currentWord.length; i++) {
 
 document.onkeyup = function (event) {
     let userInput = event.key;
-}
 
-// if userInput exists within word chosen, then it will replace the "_" at the corresponding index
+    // if userInput does not exist within word chosen, it will be added to empty array displayed in letter guessed section and guesses remaining decreases by 1
+    if (currentWord.indexOf(userInput) === -1) {
+        console.log("this letter is NOT in the word")
 
-// if userInput does not exist within word chosen, it will be added to empty array displayed in letter guessed section
-    // and guesses remaining decreases by 1
+    } else {
+        // if userInput exists within word chosen, then it will replace the "_" at the corresponding index
+        console.log("this letter IS in the word")
 
-// if userInput already exists within letters guessed, alert will ask for different input
+    }
+
+    // if userInput already exists within letters guessed, alert will ask for different input
     // guesses remaining will not decrease
 
     // once current word shows no underscores, wins increases by 1 (if indexof "_" < 0) and game resets
+
+};
+
+
+
 
 // GAME RESET: letters guessed array empties, guesses remaining returns to original value
 
